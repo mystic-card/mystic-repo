fd09360ff3efe5a690a7f7a145c7eae0e171789f
nokatotedo <dwikypanduaqri22@gmail.com>
2023-11-22 05:39:56 +0700

    update card style

--- client/mystic/src/components/Card.jsx
+++ client/mystic/src/components/Card.jsx
@@ -1,39 +1,34 @@
-import { Link } from "react-router-dom";
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
+import './Card.css'
 
-export function Card() {
+export default function Card({ card, index }) {
+  const [choosen, setChoosen] = useState(false)
+
   return (
     <>
-      <div className="md:container md:mx-auto">
-        <div className="grid md:grid-cols-3 gap-5">
-          <div className="relative flex flex-col text-gray-700 bg-white w-96 rounded-xl bg-clip-border transition-all hover:scale-105 shadow-lg shadow-blue-gray-900/50">
-            <Link to="">
-              <div className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white h-96 rounded-xl bg-clip-border">
-                <img src="/Miya.jpg" className="object-cover w-full h-full" />
-              </div>
-              <div className="p-6">
-                <div className="flex items-center justify-between mb-2">
-                  <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
-                    Miya
-                  </p>
-                  <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
-                    Marksman
-                  </p>
-                </div>
-                <p className="block font-sans text-lg text-center antialiased font-normal leading-normal text-gray-700 opacity-100">
-                  <b>Attack 22.500</b>
-                </p>
-              </div>
-            </Link>
-            <div className="p-6 pt-0">
-              <button
-                className="block w-full select-none rounded-lg bg-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                type="button"
-              >
-                Attack
-              </button>
-            </div>
+      <div className={`${choosen ? "-translate-y-10" : ""} relative flex flex-col text-gray-700 bg-white rounded-xl bg-clip-border transition-all hover:-translate-y-10 shadow-lg shadow-blue-gray-900/50 w-48 hover:z-10`}>
+        <input type="checkbox" className="select-card" id={`card-${index}`} onChange={() => {
+          choosen === true ? setChoosen(false) : setChoosen(true)
+        }}/>
+        <label htmlFor={`card-${index}`}>
+          <div className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white h-32 rounded-xl bg-clip-border">
+            <img src={card?.imageUrl} className="object-cover w-full h-full" />
           </div>
-        </div>
+          <div className="p-6">
+            <div className="flex items-center mb-2">
+              <p className="flex-1 text-left leading-5 font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+                {card?.name}
+              </p>
+              <p className="flex-1 text-right leading-5 font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+                {card?.role.replace(",", ", ")}
+              </p>
+            </div>
+            <p className="block font-sans text-lg text-center antialiased font-normal leading-normal text-gray-700 opacity-100">
+              <b>{card?.operator ? "+" : "-"}{card?.power}</b>
+            </p>
+          </div>
+        </label>
       </div>
     </>
   );
