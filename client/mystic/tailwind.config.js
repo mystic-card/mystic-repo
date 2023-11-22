dff482cbc854f19069f372d29c0c01f1527d696c
syifasrh <syifasarah19@gmail.com>
2023-11-22 13:53:29 +0700

    Done add waiting page and backsound

--- client/mystic/tailwind.config.js
+++ client/mystic/tailwind.config.js
@@ -1,16 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}",
-    './pages/**/*.{html,js}',
-    './components/**/*.{html,js}',],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        gray: colors.blueGray,
-        pink: colors.fuchsia,
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
-
