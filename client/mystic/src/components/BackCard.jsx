1a32a0a40f29e5295a0fcd3a4527f04b36319f1a
nokatotedo <dwikypanduaqri22@gmail.com>
2023-11-22 23:53:05 +0700

    add back card component as enemy card

--- client/mystic/src/components/BackCard.jsx
+++ client/mystic/src/components/BackCard.jsx
@@ -1,33 +1,9 @@
-import { useState } from "react";
-
 export function BackCard() {
-  const [choosen, setChoosen] = useState(false);
-
   return (
     <>
-      <div className="flex-row flex">
-        <div className="relative flex flex-col text-gray-700 bg-white rounded-xl bg-clip-border transition-all hover:-translate-y-10 shadow-lg shadow-blue-gray-900/50 w-48 hover:z-10">
-          <div className="card">
-            <img src="/b-card.png" alt="Card" />
-          </div>
-        </div>
-
-        <div className="relative flex flex-col text-gray-700 bg-white rounded-xl bg-clip-border transition-all hover:-translate-y-10 shadow-lg shadow-blue-gray-900/50 w-48 hover:z-10">
-          <div className="card">
-            <img src="/b-card.png" alt="Card" />
-          </div>
-        </div>
-
-        <div className="relative flex flex-col text-gray-700 bg-white rounded-xl bg-clip-border transition-all hover:-translate-y-10 shadow-lg shadow-blue-gray-900/50 w-48 hover:z-10">
-          <div className="card">
-            <img src="/b-card.png" alt="Card" />
-          </div>
-        </div>
-
-        <div className="relative flex flex-col text-gray-700 bg-white rounded-xl bg-clip-border transition-all hover:-translate-y-10 shadow-lg shadow-blue-gray-900/50 w-48 hover:z-10">
-          <div className="card">
-            <img src="/b-card.png" alt="Card" />
-          </div>
+      <div className="relative flex flex-col text-gray-700 bg-white rounded-xl bg-clip-border transition-all hover:translate-y-10 shadow-lg shadow-blue-gray-900/50 w-48 hover:z-10">
+        <div className="card">
+          <img src="/b-card.png" alt="Card" />
         </div>
       </div>
     </>
